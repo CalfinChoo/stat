c86304d10a969bc990654116134bed042736e1aa
Calvin Chu <cchu00@stuy.edu>
2019-11-13 13:46:54 -0500

    work2

--- main.c
+++ main.c
@@ -15,4 +15,25 @@ int main() {
   printf("File size: %d\n\n", fs);
   printf("File permissions: %o\n\n", fp);
   printf("Last Access: %s\n\n", p);
+  char buffer[50];
+  char metric[8];
+  int m = 1;
+  if (fs / 1000000000 > 0) {
+    sprintf(metric, "%s", "GB");
+    m = 1000000000;
+  }
+  else if (fs / 1000000 > 0) {
+    sprintf(metric, "%s", "MB");
+    m = 1000000;
+  }
+  else if (fs / 1000 > 0) {
+    sprintf(metric, "%s", "KB");
+    m = 1000;
+  }
+  else sprintf(metric, "%s", "B");
+
+  sprintf(buffer, "%d %s", fs/m, metric);
+  printf("File size: %s\n", buffer);
+  char pbuffer[40];
+  printf("Permissions: %s\n", "placeholder");
 }
